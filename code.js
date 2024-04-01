d627501d27b1e12b653cc3e88550df7c5be1dc6e
Samuel Gonzalez <sgonza21@uwyo.edu>
2024-04-01 14:13:44 -0600

    started work on quicksort, pivot working

--- code.js
+++ code.js
@@ -1,3 +1,58 @@
+const { small } = require("jsverify");
+
 function quicksort(array) {
+
+    index = pivot(array)
+
+    console.log(index + " " + array[index])
+
     return array;
 }
+
+
+//pivot fucntion will alter the array it is given 
+function pivot(array, smallerThan = -1, pointer = 0, comparing = array.length-1){
+
+    while (pointer != comparing){
+        //console.log(array[pointer] + " <= " + array[comparing] + " " + (array[pointer] <= array[comparing]))
+        if (array[pointer] <= array[comparing]){
+            smallerThan++
+            if(pointer > smallerThan){
+                temp = array[smallerThan]
+                array[smallerThan] = array[pointer]
+                array[pointer] = temp
+            }
+            if (pointer == smallerThan){
+                pointer++
+            }
+        }
+        else {
+            pointer++
+        }
+    }
+    
+    if (smallerThan >= 0) {
+        smallerThan++
+
+        if (smallerThan != comparing){
+            temp = array[smallerThan]
+            array[smallerThan] = array[comparing]
+            array[comparing] = temp
+        }    
+    }
+
+    return smallerThan
+
+}
+
+function splitArray(array, split){
+
+    rightListR = split + 1
+    rightListL = array.length-1
+    leftListR = split - 1
+    leftListL = 0
+
+}
+
+console.log(quicksort([3,2,5,0,1,8,7,6,9,4]))
+console.log(quicksort([3,2,1,0,1,1,1,1,1,4]))
