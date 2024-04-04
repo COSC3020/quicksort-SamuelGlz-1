c47852ff08414271baaccb9c943d2a70bba81eef
Samuel Gonzalez <sgonza21@uwyo.edu>
2024-04-04 11:48:52 -0600

    quicksort implemented

--- code.js
+++ code.js
@@ -1,20 +1,29 @@
-const { small } = require("jsverify");
+const { small, check } = require("jsverify");
 
 function quicksort(array) {
 
-    index = pivot(array)
+    sublists = [[0, array.length-1]]
 
-    console.log(index + " " + array[index])
+    while (sublists.length !=0) {
+
+        values = sublists.pop()
+        minimum = values[0]
+        max = values[1]
+
+        if (minimum < max) {
+            pivot(array, minimum -1, minimum, max)
+        }
+    }
 
     return array;
 }
 
-
 //pivot fucntion will alter the array it is given 
 function pivot(array, smallerThan = -1, pointer = 0, comparing = array.length-1){
 
+    base = smallerThan
+
     while (pointer != comparing){
-        //console.log(array[pointer] + " <= " + array[comparing] + " " + (array[pointer] <= array[comparing]))
         if (array[pointer] <= array[comparing]){
             smallerThan++
             if(pointer > smallerThan){
@@ -30,29 +39,27 @@ function pivot(array, smallerThan = -1, pointer = 0, comparing = array.length-1)
             pointer++
         }
     }
-    
-    if (smallerThan >= 0) {
-        smallerThan++
 
-        if (smallerThan != comparing){
-            temp = array[smallerThan]
-            array[smallerThan] = array[comparing]
-            array[comparing] = temp
-        }    
+    smallerThan++
+    if (smallerThan != comparing){
+        temp = array[smallerThan]
+        array[smallerThan] = array[comparing]
+        array[comparing] = temp
+    }   
+
+    // after this smaller than becomes a pointer to the middle of the list (the middle having the sorted part)
+    //comparing should still be the last position
+
+    if (!((smallerThan - 1) == 0)) {
+        sublists.push([(base+1),(smallerThan-1)])
+    }
+    if (!((smallerThan + 1) > (array.length - 1))) {
+        sublists.push([(smallerThan+1),(comparing)])
     }
 
     return smallerThan
 
 }
 
-function splitArray(array, split){
-
-    rightListR = split + 1
-    rightListL = array.length-1
-    leftListR = split - 1
-    leftListL = 0
-
-}
-
-console.log(quicksort([3,2,5,0,1,8,7,6,9,4]))
-console.log(quicksort([3,2,1,0,1,1,1,1,1,4]))
+//console.log(quicksort([3,2,5,0,1,8,7,6,9,4]))
+//console.log(quicksort([3,2,1,0,1,1,1,1,1,4]))
